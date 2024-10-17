77abeb633ebd30b1ec244cab30f3c1c12e2a92a1
Varsha-Bosch <varshasrinivas46@gmail.com>
2024-10-17 13:53:22 +0530

    Delete check_main.cpp

--- check_main.cpp
+++
@@ -1,9 +0,0 @@
-#include "battery_check.h"
-#include <iostream>
-using namespace std;
-
-int main() {
-    // Main function with no testing logic or sample values.
-    // This file can be used for running the battery checks when needed.
-    return 0;
-}
